a66124d07945a09f580fe68013dc08b8243fa40e
pop2021047 <pop2021047@gmail.com>
2018-11-19 15:35:15 +0800

    1

---
+++ blog/article/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+def article(request):
+    '''
+    Render the article page
+    '''
+    return render(request, 'article/article.html')
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'article',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -121,5 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
